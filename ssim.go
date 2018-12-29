62c4c3e1d65727ad1390394a6c44f49fb54c0519
Kouhei Suzuki <kouhei.szk@gmail.com>
2018-12-29 17:23:47 +0900

    分散を利用するようにして、RGB成分を別々に計算する

--- ssim.go
+++ ssim.go
@@ -16,108 +16,87 @@ var (
 	C2 = math.Pow(K2*L, 2.0)
 )
 
-func dimensions(img image.Image) (width, height int) {
+const (
+	R = 0
+	G = 1
+	B = 2
+)
+
+func pixelValue(c color.Color, component int) float64 {
+	r, g, b, _ := c.RGBA()
+	return float64([]uint32{r, g, b}[component] >> 8)
+}
+
+func isSameDimension(img1, img2 image.Image) bool {
+	bounds1 := img1.Bounds()
+	bounds2 := img2.Bounds()
+	return (bounds1.Dx() == bounds2.Dx()) && (bounds1.Dy() == bounds2.Dy())
+}
+
+func average(img image.Image, component int) float64 {
 	bounds := img.Bounds()
-	return bounds.Dx(), bounds.Dy()
-}
-
-func getPixelValue(c color.Color) float64 {
-	r, _, _, _ := c.RGBA()
-	return float64(r >> 8)
-}
-
-func equalDim(img1, img2 image.Image) bool {
-	w1, h1 := dimensions(img1)
-	w2, h2 := dimensions(img2)
-	return (w1 == w2) && (h1 == h2)
-}
-
-func convertToGrayscale(img image.Image) image.Image {
-	imageBounds := img.Bounds()
-	width, height := dimensions(img)
-
-	grayImage := image.NewGray(imageBounds)
-
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			originalColor := img.At(x, y)
-			grayColor := color.GrayModel.Convert(originalColor)
-			grayImage.Set(x, y, grayColor)
-		}
-	}
-
-	return grayImage
-}
-
-func mean(img image.Image) float64 {
-	width, height := dimensions(img)
-	n := float64((width * height) - 1)
 	sum := 0.0
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			sum += getPixelValue(img.At(x, y))
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			sum += pixelValue(img.At(x, y), component)
 		}
 	}
 
-	return sum / n
+	pixels := float64(bounds.Dx() * bounds.Dy())
+	return sum / pixels
 }
 
-func stdDev(img image.Image) float64 {
-	width, height := dimensions(img)
-
-	n := float64((width * height) - 1)
+func variance(img image.Image, avg float64, component int) float64 {
+	bounds := img.Bounds()
 	sum := 0.0
-	avg := mean(img)
-
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			pixel := getPixelValue(img.At(x, y))
-			sum += math.Pow(pixel-avg, 2.0)
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			value := pixelValue(img.At(x, y), component)
+			sum += math.Pow(value-avg, 2.0)
 		}
 	}
 
-	return math.Sqrt(sum / n)
+	pixels := float64(bounds.Dx() * bounds.Dy())
+	return sum / pixels
 }
 
-func covar(img1, img2 image.Image) (float64, error) {
-	if !equalDim(img1, img2) {
+func covariance(img1, img2 image.Image, avg1, avg2 float64, component int) float64 {
+	bounds := img1.Bounds()
+	sum := 0.0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			value1 := pixelValue(img1.At(x, y), component)
+			value2 := pixelValue(img2.At(x, y), component)
+			sum += (value1 - avg1) * (value2 - avg2)
+		}
+	}
+
+	pixels := float64(bounds.Dx() * bounds.Dy())
+	return sum / pixels
+}
+
+func ssim(img1, img2 image.Image, component int) float64 {
+	avg1 := average(img1, component)
+	avg2 := average(img2, component)
+
+	var1 := variance(img1, avg1, component)
+	var2 := variance(img2, avg2, component)
+
+	covar := covariance(img1, img2, avg1, avg2, component)
+
+	num := ((2.0 * avg1 * avg2) + C1) * ((2.0 * covar) + C2)
+	denominator := (math.Pow(avg1, 2.0) + math.Pow(avg2, 2.0) + C1) * (var1 + var2 + C2)
+	return num / denominator
+}
+
+func SSIM(img1, img2 image.Image) (float64, error) {
+	if !isSameDimension(img1, img2) {
 		return 0, fmt.Errorf("images must have the same dimension")
 	}
 
-	avg1 := mean(img1)
-	avg2 := mean(img2)
+	r := ssim(img1, img2, R)
+	g := ssim(img1, img2, G)
+	b := ssim(img1, img2, B)
 
-	width, height := dimensions(img1)
-	sum := 0.0
-	n := float64((width * height) - 1)
-
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			pix1 := getPixelValue(img1.At(x, y))
-			pix2 := getPixelValue(img2.At(x, y))
-			sum += (pix1 - avg1) * (pix2 - avg2)
-		}
-	}
-
-	return sum / n, nil
-}
-
-func SSIM(x, y image.Image) (float64, error) {
-	x = convertToGrayscale(x)
-	y = convertToGrayscale(y)
-
-	avgX := mean(x)
-	avgY := mean(y)
-
-	stdDevX := stdDev(x)
-	stdDevY := stdDev(y)
-
-	covar, err := covar(x, y)
-	if err != nil {
-		return 0, err
-	}
-
-	num := ((2.0 * avgX * avgY) + C1) * ((2.0 * covar) + C2)
-	denominator := (math.Pow(avgX, 2.0) + math.Pow(avgY, 2.0) + C1) * (math.Pow(stdDevX, 2.0) + math.Pow(stdDevY, 2.0) + C2)
-	return num / denominator, nil
+	return (r + g + b) / 3.0, nil
 }
