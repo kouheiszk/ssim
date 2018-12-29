ab69d419615a101c0a8bcf9e790c0b030efb5e21
Kouhei Suzuki <kouhei.szk@gmail.com>
2018-12-29 17:45:56 +0900

    main関数をシンプルにする

--- cmd/ssim/main.go
+++ cmd/ssim/main.go
@@ -6,36 +6,38 @@ import (
 	"image"
 	_ "image/jpeg"
 	_ "image/png"
-	"log"
 	"os"
-	"reflect"
 )
 
 func main() {
-	file, err := os.Open("../../test/pic.jpg")
+	path1 := "../../test/pic.jpg"
+	path2 := "../../test/pic2.jpg"
+
+	img1, err := loadImage(path1)
 	if err != nil {
-		log.Fatalf("Could Not Open Pic -> %v", err)
+		panic(err)
 	}
+
+	img2, err := loadImage(path2)
+	if err != nil {
+		panic(err)
+	}
+
+	ssim, err := ssim.SSIM(img1, img2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("SSIM = %f", ssim)
+}
+
+func loadImage(path string) (image.Image, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
 	img, _, err := image.Decode(file)
-	if err != nil {
-		log.Fatalf("Could Not Decode Pic -> %v", err)
-	}
-
-	file, err = os.Open("../../test/pic2.jpg")
-	if err != nil {
-		log.Fatalf("Could Not Open Pic2 -> %v", err)
-	}
-
-	img2, _, err := image.Decode(file)
-	if err != nil {
-		log.Fatalf("Could Not Decode Pic2 -> %v", err)
-	}
-	index, err := ssim.SSIM(img, img2)
-	if err != nil {
-		log.Fatalf("Could Not Calculate SSIM -> %v", err)
-	}
-
-	fmt.Printf("Index is type of = %v\n", reflect.TypeOf(index))
-	fmt.Printf("Index Value = %f", index)
+	return img, err
 }
